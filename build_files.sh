3a93443d5f6270cb448bb22a71d268ee1f03387d
RahulSaini78 <rs5778882@gmail.com>
2023-04-26 14:30:42 +0530

    change python path

--- build_files.sh
+++ build_files.sh
@@ -2,8 +2,9 @@
 
 echo "BUILD START"
 
-python3.8 -m pip install -r requirements.txt
-python3.8 manage.py collectstatic --noinput --clear
+C:/Program\ Files/Python311/python.exe -m pip install -r requirements.txt
+C:/Program\ Files/Python311/python.exe manage.py collectstatic --noinput --clear
 
 echo "BUILD END"
+
 
