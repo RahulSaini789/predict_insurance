7b6837d2b2ff3d57618f1b813f146e6312ce489d
RahulSaini78 <rs5778882@gmail.com>
2023-04-28 16:21:51 +0530

    psycopg2 Database add

--- predict_insurance/settings_rail.py
+++ predict_insurance/settings_rail.py
@@ -3,3 +3,16 @@ from predict_insurance.settings import *
 from decouple import config
 
 SECRET_KEY = config('SECRET_KEY')
+
+ALLOWED_HOSTS =  [web-production-0558.up.railway.app]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config('DATABASE_NAME'),
+        'USER': config('DATABASE_USER'),
+        'PASSWORD': config('DATABASE_PASSWORD'),
+        'HOST': config('DATABASE_HOST'),
+        'PORT': config('DATABASE_PORT'),
+    }
+}
